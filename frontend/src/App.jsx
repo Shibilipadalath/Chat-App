c15b1102d7baba44f765268ecd50126866faa734
Shibilipadalath <shibilipadalath@gmail.com>
2025-05-20 23:15:04 +0530

    signup page and login,logout,navbar all completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import ProfilePage from './pages/ProfilePage'
 import { useAuthStore } from './store/useAuthStore.js'
 
 import {Loader} from 'lucide-react'
+import { Toaster } from 'react-hot-toast'
 
 function App() {
   const {authUser,checkAuth,isCheckingAuth} = useAuthStore()
@@ -40,6 +41,8 @@ function App() {
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser?<ProfilePage />: <Navigate to="/login"/>} />
       </Routes>
+
+      <Toaster/>
     </div>
   )
 }
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
   authUser: null,
@@ -23,4 +24,42 @@ export const useAuthStore = create((set) => ({
       set({ isCheckingAuth: false });
     }
   },
+
+  signup: async (data) => {
+    set({isSigningUp: true});
+    try{
+      const res=await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.success("Account created successfully");
+    }catch (error) {
+      toast.error(error.response.data.message || "Something went wrong");
+    }finally{
+      set({isSigningUp: false});
+    }
+  },
+
+  logout:async()=>{
+    try{
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    }catch (error) {
+      toast.error(error.response.data.message || "Something went wrong");
+    }
+  },
+
+  login:async (data) => {
+    set({isLoggingIn: true});
+    try{
+      const res=await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+      toast.success("Logged in successfully");
+    }catch (error) {
+      toast.error(error.response.data.message || "Something went wrong");
+    }finally{
+      set({isLoggingIn: false});
+    }
+  }
+  
+
 }));
