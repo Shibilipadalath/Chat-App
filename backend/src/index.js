1a07c6aa58f891ad7f28dea159a4ac22eca106ad
Shibilipadalath <shibilipadalath@gmail.com>
2025-05-23 09:11:40 +0530

    profile page and updating completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,9 @@ const app = express();
 
 const PORT=process.env.PORT
 
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ extended: true, limit: '10mb' }))
+
 app.use(cookesParser())
 app.use(cors({
   origin:  "http://localhost:5173",
