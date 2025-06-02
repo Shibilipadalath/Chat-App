90a01bc1cc0ecd270baf298f0ff92a8338f9f6a3
Shibilipadalath <shibilipadalath@gmail.com>
2025-06-02 19:40:20 +0530

    make an updation on the version of daisyui duo to the issue in of background color chage

--- frontend/src/components/AuthImagePattern.jsx
+++ frontend/src/components/AuthImagePattern.jsx
@@ -1,4 +1,4 @@
-function AuthImagePattern({ title, subtitle }) {
+const AuthImagePattern = ({ title, subtitle }) => {
   return (
     <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
       <div className="max-w-md text-center">
@@ -6,7 +6,7 @@ function AuthImagePattern({ title, subtitle }) {
           {[...Array(9)].map((_, i) => (
             <div
               key={i}
-              className={`aspect-square rounded-2xl bg-blue-950 ${
+              className={`aspect-square rounded-2xl bg-primary/10 ${
                 i % 2 === 0 ? "animate-pulse" : ""
               }`}
             />
@@ -16,7 +16,7 @@ function AuthImagePattern({ title, subtitle }) {
         <p className="text-base-content/60">{subtitle}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AuthImagePattern
+export default AuthImagePattern;
