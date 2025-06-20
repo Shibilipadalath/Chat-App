de3b95026aa5560f11e9dbf80e8417fc9ea1cd9b
Shibilipadalath <shibilipadalath@gmail.com>
2025-06-20 10:33:20 +0530

    generate token updated for production time

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -7,7 +7,7 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     maxAge: 7 * 24 * 60 * 60 * 1000,
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "development" ? "Lax" : "None",
     secure: process.env.NODE_ENV !== "development",
   });
   return token;
